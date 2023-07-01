65fb30018eef3b4f5c07bb9cc6350823695073a0
Florencecoker <damselpiccolo18@gmail.com>
2023-07-01 01:33:13 +0000

    done

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,9 +1,21 @@
-export default function createReportObject(employeesList) {
-  const allEmployees = {};
+export default function iterateThroughObject(reportWithIterator) {
 
-  for (const [departmentName, employees] of Object.entries(employeesList)) {
-    allEmployees[departmentName] = [...employees];
-  }
+ {
+  allEmployees: {
+     engineering: [
+          'John Doe',
+          'Guillaume Salva',
+     ],
+  },
+ employees = {
+      ...createEmployeesObject('engineering', engineering),
+      ...createEmployeesObject('design', design),
+    };
+
+ const report = createReportObject(employees);
+ const reportWithIterator = createIteratorObject(report);
+ iterateThroughObject(reportWithIterator)
+'John Doe | Guillaume Salva | Kanye East | Jay Li'
 
   return {
     allEmployees,
